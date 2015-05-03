b64d1e21886c640cc37ffffc288fb46547546c79
Rod Blackney <rblackney@gmail.com>
2015-05-03 20:13:07 +1000

    adjusted form elements

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$('input[name="firstname"]).val()
+
+
+$('ul').on('click', 'li', function(event){
+  // fires when any LIs are clicked on
+  // including LIs that aren't on the page when it is initially loaded
+});
