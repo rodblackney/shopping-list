9b34ad654bf9986425036794fd51b5c725092add
Rod Blackney <rblackney@gmail.com>
2015-05-10 19:03:57 +1000

    added changes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-
 	$('#add-button').click(function(){
     	var userInput = $("#new-item").val();
 		console.log("log message" , userInput);
 	});
-
 });
 
+ var added-items = $('added-items'</li>');
