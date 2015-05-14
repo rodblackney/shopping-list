001bb5ed9045578a9b9869109b4a3a5bc252cb6e
Rod Blackney <rblackney@gmail.com>
2015-05-14 20:49:57 +1000

    updated js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$('#add-button').click(function(){
     	var userInput = $("#new-item").val();
 		console.log("log message" , userInput);
+		$('#shopping-list').append('<li>' + userInput + '</li>');
+		return false;
 	});
 });
-
- var added-items = $('added-items'</li>');
