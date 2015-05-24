3ee79a37a97f4d70a65139daad3ac98da402a5eb
Rod Blackney <rblackney@gmail.com>
2015-05-24 19:46:44 +1000

    updated js file

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
-$('#new-value').on('keypress', function(evt) {
-  if (evt.keyCode === 13) {
-    addItem();
+$(document).ready(function() {
+  $('#new-value').on('keypress', function(evt) {
+    if (evt.keyCode === 13) {
+      addItem();
+    }
+  });
+
+  $('#add-item').on('click', addItem);
+
+  $('#clear-item').on('click', function() {
+    var checked = $('#shopping-list input:checked'); // select all checked checkboxes
+    var listItem = checked.parent(); // the LI element
+    listItem.remove(); // remove the LI element that contains the checked checkbox
+  });
+
+  function addItem() {
+    var newItem = $('#new-value').val();
+    if (newItem === '') {
+      return;
+    }
+    $('#shopping-list').append('<li><input type="checkbox" /> ' + newItem + '</li>');
+    $('#new-value').val('');
   }
 });
-
-$('#add-item').on('click', addItem);
-
-$('#clear-item').on('click', function() {
-  var checked = $('#shopping-list input:checked'); // select all checked checkboxes
-  var listItem = checked.parent(); // the LI element
-  listItem.remove(); // remove the LI element that contains the checked checkbox
-});
-
-function addItem() {
-  var newItem = $('#new-value').val();
-  if (newItem === '') {
-    return;
-  }
-  $('#shopping-list').append('<li><input type="checkbox" /> ' + newItem + '</li>');
-  $('#new-value').val('');
-}
