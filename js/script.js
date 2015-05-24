ec5807a0249d01531e90c1afca2fadb19e221178
Rod Blackney <rblackney@gmail.com>
2015-05-24 19:27:50 +1000

    updated page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-$('input[name="firstname"]).val()
-
-
-$('ul').on('click', 'li', function(event){
-  // fires when any LIs are clicked on
-  // including LIs that aren't on the page when it is initially loaded
+$('#new-value').on('keypress', function(evt) {
+  if (evt.keyCode === 13) {
+    addItem();
+  }
 });
+
+$('#add-item').on('click', addItem);
+
+$('#clear-item').on('click', function() {
+  var checked = $('#shopping-list input:checked'); // select all checked checkboxes
+  var listItem = checked.parent(); // the LI element
+  listItem.remove(); // remove the LI element that contains the checked checkbox
+});
+
+function addItem() {
+  var newItem = $('#new-value').val();
+  if (newItem === '') {
+    return;
+  }
+  $('#shopping-list').append('<li><input type="checkbox" /> ' + newItem + '</li>');
+  $('#new-value').val('');
+}
